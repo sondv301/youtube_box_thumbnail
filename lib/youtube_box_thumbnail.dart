bd18800a8ee96a23e80af7a9d931b3731b58bdd7
Dương Văn Sơn <sondv@aladdin-jsc.com>
2024-06-24 09:43:40 +0700

    update

--- lib/youtube_box_thumbnail.dart
+++ lib/youtube_box_thumbnail.dart
@@ -45,9 +45,7 @@ class _YoutubeBoxThumbnailState extends State<YoutubeBoxThumbnail> {
         future: youtubeVideo,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return InkWell(
-              child: _YoutubeVideoWidget(youtubeVideo: snapshot.data!),
-            );
+            return _YoutubeVideoWidget(youtubeVideo: snapshot.data!);
           } else if (snapshot.hasError) {
             return Text("error");
           } else {
@@ -68,17 +66,22 @@ class _YoutubeBoxThumbnailState extends State<YoutubeBoxThumbnail> {
       return YoutubeVideo(
         widget.url,
         title,
-        "imageUrl",
+        "https://img.youtube.com/vi/${_getIdFromYoutubeUrl()}/0.jpg",
       );
     } catch (ex) {
       rethrow;
     }
   }
 
-  Future<void> _launcherVideo() async {
-    if (await launchUrlString(widget.url)) {
-      //
+  String? _getIdFromYoutubeUrl() {
+    for (var exp in [
+      RegExp(r".*\?v=(.+?)&.+"),
+      RegExp(r"^https:\/\/youtu\.be\/([_\-a-zA-Z0-9]{11}).*$")
+    ]) {
+      Match? match = exp.firstMatch(widget.url);
+      if (match != null && match.groupCount >= 1) return match.group(1);
     }
+    return null;
   }
 }
 
@@ -90,7 +93,7 @@ class _LoadingWidget extends StatelessWidget {
     return Row(
       children: [
         Container(
-          width: 60,
+          width: 80,
           height: 60,
           decoration: BoxDecoration(
             color: Colors.grey.shade200,
@@ -119,31 +122,57 @@ class _YoutubeVideoWidget extends StatelessWidget {
   final YoutubeVideo youtubeVideo;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          width: 60,
-          height: 60,
-          decoration: BoxDecoration(
-            color: Colors.grey.shade200,
-            borderRadius: _kBorderRadius,
+    return InkWell(
+      onTap: () async {
+        // await launchUrlString(youtubeVideo.url);
+      },
+      child: Row(
+        children: [
+          Container(
+            width: 80,
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: _kBorderRadius,
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage(
+                  youtubeVideo.imageUrl,
+                ),
+              ),
+            ),
+            child: Icon(
+              Icons.play_circle_outline,
+              color: Colors.red[700],
+            ),
           ),
-          child: const Icon(
-            Icons.play_circle_outline,
-            color: Colors.grey,
+          const SizedBox(
+            width: 12,
           ),
-        ),
-        const SizedBox(
-          width: 12,
-        ),
-        Expanded(
-          child: Column(
-            children: [
-              Text(youtubeVideo.title),
-            ],
+          Expanded(
+            child: Column(
+              children: [
+                Text(
+                  youtubeVideo.title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  youtubeVideo.url,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
